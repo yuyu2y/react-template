227cb93987e0e7ac0d0d7b8ee8c0f5e8d5c26c96
yuyu2 <yuyu2yCo@gmail.com>
2024-07-17 22:58:11 +0900

    fix any type

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { useState  } from "react";
+import { useState } from "react";
 import { RadioButton } from "./components/radioButton";
 
 const App = () => {
-    const [ selectedOption, setSelectedOption ] = useState('');
-    const options = [ "option 1","option 2"];
- const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(event.target.value)
+  const [selectedOption, setSelectedOption] = useState("");
+  const options = ["option 1", "option 2"];
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
   };
 
-return(
+  return (
     <div>
-    <RadioButton
-      options={options}
-      selectedOption={selectedOption}
-      onChange={handleOptionChange}
-    />
-  </div>
-  
-);
+      <RadioButton
+        options={options}
+        selectedOption={selectedOption}
+        onChange={handleOptionChange}
+      />
+    </div>
+  );
 };
 export default App;
--- src/components/radioButton.tsx
+++ src/components/radioButton.tsx
@@ -1,28 +1,25 @@
 type Select = {
-    options: string[],
-    selectedOption: string,
-    onChange: any
+  options: string[];
+  selectedOption: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-export const RadioButton =(props: Select)=>{
-    const {options,selectedOption,onChange} =props;
-    
-     return (
-         <div>
-         
-             {options.map((option) => (
-                  <label key={option}>
-                      <input
-                         type="radio"
-                         value={option}
-                         name="select"
-                         checked={selectedOption === option}
-                         onChange={onChange}
-                     />
-                 {option}
-                 </label>
-                 ))}
-             
-     
-     </div>
-     );
- };
+export const RadioButton = (props: Select) => {
+  const { options, selectedOption, onChange } = props;
+
+  return (
+    <div>
+      {options.map((option) => (
+        <label key={option}>
+          <input
+            type="radio"
+            value={option}
+            name="select"
+            checked={selectedOption === option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </div>
+  );
+};
